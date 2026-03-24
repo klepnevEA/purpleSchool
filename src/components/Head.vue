6812e1c583533328618b7d8b07e7e5e32c8a8180
klepnev-ea <klepnev-ea>
2026-03-24 21:06:30 +0300

    3-events

---
+++ src/components/Head.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import {ref} from "vue";
+    import Count from "./ui/Count.vue";
+    const countStore = ref(100);
+</script>
+
+<template>
+ <header class="head">
+    <div class="head__title">ЗАПОМНИ СЛОВО</div>
+    <Count :count="countStore" />
+ </header>
+</template>
+
+<style scoped>
+.head {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 27px 62px;
+}
+
+.head__title {
+    text-transform: uppercase;
+    color: var(--secondary-color);
+    font-weight: 700;
+}
+</style>
