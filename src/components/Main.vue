22c6c4c703537e804e7a5a212d3ffb1b62bd2ee6
klepnev-ea <klepnev-ea>
2026-03-27 15:41:45 +0300

    7-final

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,15 @@
 <script setup>
     import { inject } from "vue";
+    import Button from "./ui/Button.vue";
     import Card from "./Card.vue";
     const cardsList = inject("cardsList");
+
+
+    const emit = defineEmits(['newGame'])
+    
+    const newGame = () => {
+        emit('newGame')
+    }
 </script>
 
 <template>
@@ -9,7 +17,11 @@
  <template v-for="(item, index) in cardsList" :key="index">
      <Card  :cardInfo="item" :id="index" />
  </template>
+
 </main>
+<div class="footer">
+     <Button @click="newGame()">Начать зано</Button>
+</div>
 </template>
 
 <style scoped>
@@ -20,5 +32,11 @@
     gap: 49px;
     padding: 37px;
     justify-items: center;
+}
+
+.footer {
+    display: flex;
+    justify-content: center;
+    padding: 37px;
 }
 </style>
